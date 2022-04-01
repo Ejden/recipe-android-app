6bfaa464efed01bfb26278d71b2d860c82f8f797
Adrian <adrian.stypinski@gmail.com>
2022-04-01 17:45:35 +0200

    Home screen part implementation

--- app/src/main/java/pl/edu/wat/recipeapp/views/home/HomeView.kt
+++ app/src/main/java/pl/edu/wat/recipeapp/views/home/HomeView.kt
@@ -1,14 +1,80 @@
 package pl.edu.wat.recipeapp.views.home
 
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.Scaffold
 import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
 import androidx.hilt.navigation.compose.hiltViewModel
 import kotlinx.coroutines.flow.collect
+import pl.edu.wat.recipeapp.domain.Recipe
+import pl.edu.wat.recipeapp.domain.RecipeDifficulty
+import pl.edu.wat.recipeapp.domain.RecipeId
 import pl.edu.wat.recipeapp.views.home.views.EmptyRecipesView
-import pl.edu.wat.recipeapp.views.home.views.FirstRecipeView
+import pl.edu.wat.recipeapp.views.home.views.FirstRecipeItemView
+import pl.edu.wat.recipeapp.views.home.views.RecipeItemView
+import java.util.UUID
+
+private val items = listOf(
+    Recipe(
+        id = RecipeId(UUID.randomUUID()),
+        name = "Spaghetti bolognese",
+        difficulty = RecipeDifficulty.MEDIUM,
+        cookingTime = 40,
+        portions = 4
+    ),
+    Recipe(
+        id = RecipeId(UUID.randomUUID()),
+        name = "Spaghetti bolognese",
+        difficulty = RecipeDifficulty.MEDIUM,
+        cookingTime = 40,
+        portions = 4
+    ),
+    Recipe(
+        id = RecipeId(UUID.randomUUID()),
+        name = "Spaghetti bolognese",
+        difficulty = RecipeDifficulty.MEDIUM,
+        cookingTime = 40,
+        portions = 4
+    ),
+    Recipe(
+        id = RecipeId(UUID.randomUUID()),
+        name = "Spaghetti bolognese",
+        difficulty = RecipeDifficulty.MEDIUM,
+        cookingTime = 40,
+        portions = 4
+    ),
+    Recipe(
+        id = RecipeId(UUID.randomUUID()),
+        name = "Spaghetti bolognese",
+        difficulty = RecipeDifficulty.MEDIUM,
+        cookingTime = 40,
+        portions = 4
+    ),
+    Recipe(
+        id = RecipeId(UUID.randomUUID()),
+        name = "Spaghetti bolognese",
+        difficulty = RecipeDifficulty.MEDIUM,
+        cookingTime = 40,
+        portions = 4
+    ),Recipe(
+        id = RecipeId(UUID.randomUUID()),
+        name = "Spaghetti bolognese",
+        difficulty = RecipeDifficulty.MEDIUM,
+        cookingTime = 40,
+        portions = 4
+    ),Recipe(
+        id = RecipeId(UUID.randomUUID()),
+        name = "Spaghetti bolognese",
+        difficulty = RecipeDifficulty.MEDIUM,
+        cookingTime = 40,
+        portions = 4
+    )
+)
 
 @Composable
 fun HomeView(
@@ -16,6 +82,7 @@ fun HomeView(
 ) {
     val recipes = viewModel.recipes.collectAsState(initial = emptyList())
     val scaffoldState = rememberScaffoldState()
+    val scrollState = rememberScrollState()
     LaunchedEffect(key1 = true) {
         viewModel.uiEvent.collect { event ->
             
@@ -24,10 +91,24 @@ fun HomeView(
     Scaffold(
         scaffoldState = scaffoldState
     ) {
-        if (recipes.value.isEmpty()) {
+        if (!recipes.value.isEmpty()) {
             EmptyRecipesView()
         } else {
-            FirstRecipeView(recipe = recipes.value.first())
+            LazyColumn(
+                modifier = Modifier
+                    .fillMaxWidth()
+            ) {
+                item {
+                    FirstRecipeItemView(recipe = items.first()) {
+                        viewModel.onEvent(HomeEvent.ShowRecipe(it))
+                    }
+                }
+                if (items.size > 1) {
+                    items(items.size - 1) { index ->
+                        RecipeItemView(recipe = items[index + 1])
+                    }
+                }
+            }
         }
     }
 }
--- app/src/main/java/pl/edu/wat/recipeapp/views/home/views/FirstRecipeView.kt
+++ app/src/main/java/pl/edu/wat/recipeapp/views/home/views/FirstRecipeView.kt
@@ -1,30 +1,81 @@
 package pl.edu.wat.recipeapp.views.home.views
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import pl.edu.wat.recipeapp.R
 import pl.edu.wat.recipeapp.domain.Recipe
 import pl.edu.wat.recipeapp.domain.RecipeDifficulty
 import pl.edu.wat.recipeapp.domain.RecipeId
+import pl.edu.wat.recipeapp.ui.theme.LightGray
+import pl.edu.wat.recipeapp.ui.theme.White
+import pl.edu.wat.recipeapp.ui.theme.spacing
 import java.util.UUID
 
 @Composable
-fun FirstRecipeView(recipe: Recipe) {
-    Image(painter = painterResource(id = R.drawable.spaghetti_bolognese), contentDescription = recipe.name)
-    Text(text = "Hello")
+fun FirstRecipeItemView(
+    recipe: Recipe,
+    onRecipeClick: (Recipe) -> Unit
+) {
+    Column(
+        modifier = Modifier
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.spaghetti_bolognese),
+            contentDescription = recipe.name,
+            modifier = Modifier.fillMaxHeight(0.8f),
+            alignment = Alignment.Center
+        )
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(100.dp)
+                .padding(horizontal = MaterialTheme.spacing.medium)
+                .offset(0.dp, -MaterialTheme.spacing.large)
+                .clip(MaterialTheme.shapes.medium)
+                .background(LightGray)
+                .padding(MaterialTheme.spacing.medium)
+                .clickable {
+                    onRecipeClick(recipe)
+                }
+        ) {
+            Text(
+                text = recipe.name,
+                style = MaterialTheme.typography.h1,
+                color = White
+            )
+        }
+    }
 }
 
 @Preview
 @Composable
 fun DefaultFirstRecipeView() {
-    FirstRecipeView(recipe = Recipe(
+    FirstRecipeItemView(recipe = Recipe(
         id = RecipeId(UUID.randomUUID()),
         name = "Spaghetti bolognese",
         difficulty = RecipeDifficulty.MEDIUM,
         cookingTime = 40,
         portions = 4
-    ))
+    )) {
+
+    }
 }
